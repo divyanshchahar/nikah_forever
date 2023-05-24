d8139afdf30abdb298003d0a57a43488b0a9d93f
Divyansh Chahar <divyanshchahar@outlook.com>
2023-05-24 19:25:45 +0530

    refactored componenet

--- src/componenets/BackgroundVideoComponent.js
+++ src/componenets/BackgroundVideoComponent.js
@@ -1,19 +1,16 @@
 import backgroundVideo from "../assets/videos/background_video.mp4";
-import styles from "./BackgroundVideoComponent.module.css";
+// import styles from "./BackgroundVideoComponent.module.css";
 
 function BackgroundVideoComponent() {
   return (
-    <>
-      <div className={styles.container}>
-        <video
-          src={backgroundVideo}
-          autoPlay
-          muted
-          loop
-          className={styles.video}
-        />
+    <div className="card text-bg-dark rounded-0 border-0">
+      <video src={backgroundVideo} autoPlay muted loop />
+      <div className="card-img-overlay d-flex justify-content-center">
+        <h1 className="align-self-center text-center">
+          India’s most trusted Muslim Matrimony platform
+        </h1>
       </div>
-    </>
+    </div>
   );
 }
 
