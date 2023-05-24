013c1eecf7a5cd67519e84d31626284021b6526f
Divyansh Chahar <divyanshchahar@outlook.com>
2023-05-24 19:54:20 +0530

    changed styling

--- src/componenets/FeatureCardComponent.js
+++ src/componenets/FeatureCardComponent.js
@@ -2,16 +2,21 @@ import React from "react";
 
 function FeaturecardComponent({ featureIcon, featureName, fetureDescription }) {
   return (
-    <div className="container mt-5">
+    <div className="container my-5  rounded-5 p-1 shadow">
       <div className="d-flex justify-content-center">
         <i
           className={`${featureIcon} text-center`}
-          style={{ fontSize: "10rem" }}
+          style={{ fontSize: "10rem", color: "rgb(255 84 79)" }}
         />
       </div>
 
       <h1 className="text-center fw-bold">{featureName}</h1>
-      <p className="fs-3 text-center">{fetureDescription}</p>
+      <p
+        className="fs-3  fw-bold text-center"
+        style={{ color: "rgb(136 136 136)" }}
+      >
+        {fetureDescription}
+      </p>
     </div>
   );
 }
